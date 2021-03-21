b24e7733b511b2c23b096f335ef10bb8b139c8d4
Yalovoi <64505068+Yalovoi@users.noreply.github.com>
2021-03-21 15:47:50 -0700

    added text

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,14 +1,12 @@
 import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
 
-describe('My Login application', () => {
-    it('should login with valid credentials', () => {
+describe('Auth', () => {
+    it('user logs in with valid data', () => {
         LoginPage.open();
-
-        LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        expect(SecurePage.flashAlert).toBeExisting();
-        expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+        LoginPage.setLogin('xiligap274@heroulo.com');
+        LoginPage.setPassword('123456789');
+        LoginPage.clickSubmitButton();
+        ProfilePage.isOpen();
     });
 });
 
