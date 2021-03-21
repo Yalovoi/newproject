b0600faf77d1655651a2b9fe308c473d985e4386
Yalovoi <64505068+Yalovoi@users.noreply.github.com>
2021-03-21 15:56:41 -0700

    added code

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,15 +1,20 @@
 import Page from './page';
 
 class LoginPage extends Page {
-    get inputUsername () { return $('#normal_login_email')}
-    get inputPassword () { return $('#normal_login_password') }
-    get btnSubmit () { return $('.login-form-button') }
+    get inputUsername () { return $('#normal_login_email');}
+    get inputPassword () { return $('#normal_login_password'); }
+    get buttonSubmit () { return $('.login-form-button'); }
+
     setLogin(email){
         this.inputUsername.setValue(email);
     }
 
     setPassword(password){
-        this.inputUsername.setValue(password);
+        this.inputPassword.setValue(password);
+    }
+
+    clickSubmitButton(){
+        this.buttonSubmit.click();
     }
 
     open () {
